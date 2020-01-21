43ac1f8e8aaf7a72fe87aa107a27e8338c2f3b4b
ddupg <ddupgs@gmail.com>
2020-01-21 15:30:07 +0800

    mod

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 'use strict';
 
 hexo.config.custom_category_generator = Object.assign({
-  per_page: hexo.config.custom_category_generator.per_page || hexo.config.category_generator.per_page || hexo.config.per_page || 10
+  per_page: (hexo.config.custom_category_generator && hexo.config.custom_category_generator.per_page)
+   || (hexo.config.category_generator && hexo.config.category_generator.per_page )
+   || hexo.config.per_page || 10,
+  order_by: (hexo.config.custom_category_generator && hexo.config.custom_category_generator.order_by)
+  || (hexo.config.category_generator && hexo.config.category_generator.order_by )
+  || hexo.config.order_by || '-date',
 }, hexo.config.custom_category_generator);
 
 hexo.extend.generator.register('category', require('./lib/generator'));
