8b880228e636391ab7568607505e39086cb2a57b
ddupg <ddupgs@gmail.com>
2020-01-21 15:04:12 +0800

    mod

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 hexo.config.custom_category_generator = Object.assign({
-  per_page: config.custom_category_generator.per_page || config.category_generator.per_page || config.per_page || 10
+  per_page: hexo.config.custom_category_generator.per_page || hexo.config.category_generator.per_page || hexo.config.per_page || 10
 }, hexo.config.custom_category_generator);
 
 hexo.extend.generator.register('category', require('./lib/generator'));
